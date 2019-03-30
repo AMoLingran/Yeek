666d10542d5c15ca0ed35a075af43702242f8a0a
Moreant <552191481@qq.com>
2019-03-30 16:26:11 +0800

    update 0330

--- weekendwork/index.php
+++ weekendwork/index.php
@@ -16,11 +16,11 @@ if (PATH_SEPARATOR == ':') {
     $domain = '10.50.43.44';
 }
 $counter = 1;
-$list = [['Subject' => '数学', 'info' => '课本 P49 2(2)(4)、3(2)(4) 写本子上，上课交 ', 'annex' => ''],
-    ['Subject' => '英语', 'info' => 'B级准备课堂表演节目，女生之后从713开始，节目单发学委', 'annex' => ''],
-    ['Subject' => 'Java', 'info' => '抽象类、接口的定义和使用，写本子上，上课交', 'annex' => 'java - 实验二.ppt'],
-    ['Subject' => 'SQL server', 'info' => '做ER图，做完上交到sqlwork', 'annex' => 'SQL第二次作业下星期三交学委.docx'],
-    ['Subject' => 'php', 'info' => '①3.21的课堂笔记复写 ②在线练习可以做到第四', 'annex' => '1809.3.21.txt']
+$list = [['Subject' => '数学', 'info' => '课本 P49 2(2)(4)、3(2)(4) 写本子上，上课交。', 'annex' => ''],
+    ['Subject' => '英语', 'info' => 'B级准备课堂表演节目，713下下周五（4月12）上台。', 'annex' => ''],
+    ['Subject' => 'Java', 'info' => '课本P97填空题 带题目抄写本子上，上课交。', 'annex' => ''],
+    ['Subject' => 'SQL server', 'info' => '课本P100页 3.实训过程 写在word文档在本站上交.', 'annex' => ''],
+    ['Subject' => 'php', 'info' => '①复写28日的课堂笔记 ②在线练习可以做到第四个。', 'annex' => '1809.3.28.txt']
 ];
 ?>
 
@@ -28,7 +28,7 @@ $list = [['Subject' => '数学', 'info' => '课本 P49 2(2)(4)、3(2)(4) 写本�
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>周末作业 demo</title>
+    <title>周末作业 0330</title>
     <link rel="icon" href="/logo.png" sizes="32x32">
     <link href="/navAndFooter.css" type="text/css" rel="stylesheet"/>
     <link href="default.css" type="text/css" rel="stylesheet"/>
@@ -38,7 +38,7 @@ $list = [['Subject' => '数学', 'info' => '课本 P49 2(2)(4)、3(2)(4) 写本�
 <header>
     <div><p>Software1809</p>
         <p><span>周 末 作 业</span></p>
-        <p>0323 清单和课件</p>
+        <p>0330 清单和课件</p>
         <p><?php echo $hostname ?>版</p>
     </div>
 </header>
@@ -46,7 +46,7 @@ $list = [['Subject' => '数学', 'info' => '课本 P49 2(2)(4)、3(2)(4) 写本�
     <div>
         <?php foreach ($list as $item) {
             echo "<p id=$counter>$counter. " . $item['Subject'] . '：</p>';
-            echo '<p>' . $item['info'] . '</p><a href="annex/' . $item['annex'] . '">' . $item['annex'] . '</a>';
+            echo '<p>' . $item['info'] . '</p><a href="annex/' . $item['annex'] . '" download>' . $item['annex'] . '</a>';
             $counter++;
         }
         ?>
@@ -54,7 +54,7 @@ $list = [['Subject' => '数学', 'info' => '课本 P49 2(2)(4)、3(2)(4) 写本�
     <p id="OS">
         此页的：
         <a href="http://www.php.cn/blog/detail/11666.html">在线部分源码</a>
-        <a href="weekendwork.zip">所有源码</a>
+        <a href="https://github.com/AMoLingran/Yeek">在GitHub中</a>
     </p>
 </main>
 <?php echo include("../part/sqlfooter.php") ?>
