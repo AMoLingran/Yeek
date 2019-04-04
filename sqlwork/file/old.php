7262bcc57ae5a6cf5c42f8ef5460a8cb3cdae83a
Moreant <552191481@qq.com>
2019-04-04 15:05:17 +0800

    add old.php

---
+++ sqlwork/file/old.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MoLingran
+ * Date: 3/16 0016
+ * Time: 16:06
+ */
+$counter=0;
+$list =array();
+$path = __DIR__;
+$result = glob($path . '/*.*');
+foreach ($result as $item) {
+    if (PATH_SEPARATOR == ':') {
+        $list[] = str_replace($path . "/", "", iconv('GBK', 'UTF-8', $item));
+    } else {
+        $list[] = str_replace($path . "/", "", iconv('GBK', 'UTF-8', $item));
+    }
+}
+array_pop($list);
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <style>
+        fieldset{
+            margin: 5%;
+        }
+        legend{
+           text-align: center;
+        }
+        table, th, td {
+            border: 1px solid #4c4c4c;
+            padding: 10px;
+        }
+
+        table {
+            border-collapse: collapse;
+            text-align: center;
+            margin: auto;
+        }
+
+        tbody td:hover {
+            background-color: #efefef;
+        }
+
+    </style>
+</head>
+<body>
+<fieldset>
+    <legend>下载 <span><?php echo __DIR__ ?></span> 的文件</legend>
+    <br><br>
+    <table>
+        <tr>
+            <?php foreach ($list as $item) : $counter++; ?>
+                <td><?php echo "<a href='file/$item'>$item"; ?></a></td>
+                <?php if ($counter % 5 == 0) echo "<tr></tr>"; endforeach; ?>
+        </tr>
+    </table>
+    <br><br>
+</fieldset>
+
+</body>
+</html>
