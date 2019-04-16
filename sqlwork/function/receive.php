465a3d68c005ad3127199258cf4d988f53fe2895
Moreant <552191481@qq.com>
2019-04-17 01:18:14 +0800

    Optimize if $_FILES is empty

--- sqlwork/function/receive.php
+++ sqlwork/function/receive.php
@@ -8,10 +8,13 @@
 
 include_once("../../part/encoding.php");
 
-if(isset($_FILES))
-$fileInfo = $_FILES['upload'];
-if (move_uploaded_file($fileInfo['tmp_name'], "../file/" . encoding($fileInfo['name']))) {
-    echo '成功同步到外网';
+if (isset($_FILES)&&!empty($_FILES)) {
+    $fileInfo = $_FILES['upload'];
+    if (move_uploaded_file($fileInfo['tmp_name'], "../file/" . encoding($fileInfo['name']))) {
+        echo '成功同步到外网';
+    } else {
+        echo '同步外网失败，错误代码：' . $_FILES['file']['error'];
+    }
 }else{
-    echo '同步外网失败，错误代码：'.$_FILES['file']['error'];
+    echo "文件为空，接收上传失败";
 }
