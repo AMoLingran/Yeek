eb62407ec543f154fb275c9f2a128cb1d4e051d5
Moreant <552191481@qq.com>
2019-04-17 20:42:33 +0800

    Optimize "if" for encoding

--- part/encoding.php
+++ part/encoding.php
@@ -1,11 +1,13 @@
 <?php
-
+var_dump(PHP_OS);
 function encoding($str)
 {
     if (is_string($str)) {
-        if (PATH_SEPARATOR == ':') {
+//        if (PATH_SEPARATOR == ':') {
+        if (PHP_OS == 'Linux') {
             return $str;
         } else {
+            //PHP_OS在Windows输出WINNT
             return iconv('UTF-8', 'GBK', $str);
         }
     } else {
