ed0332b7f0382245f115ef4f3c400c3b5c42a953
Moreant <552191481@qq.com>
2019-05-23 01:46:55 +0800

    dev database

--- worklist/dbTest.php
+++ worklist/dbTest.php
@@ -1,1 +1,23 @@
 <?php
+function setPdo(){
+    $dbType="mysql";
+    $dbHost="localhost";
+    $dbName="yeek";
+    $dbUsername="moreant";
+    $dbPassword="moreant";
+    $dbCharset="utf8";
+    $dsn="$dbType:host=$dbHost;dbname=$dbName;charset=$dbCharset";
+    try{
+        $pdo = new PDO($dsn,$dbUsername,$dbPassword);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+        return $pdo;
+    }catch (PDOException $error){
+        exit("错误".$error->getMessage());
+    }
+}
+
+$pdo = setPdo();
+
+$result = $pdo->query("SELECT * FROM work ");
+var_dump($result->fetchAll());
